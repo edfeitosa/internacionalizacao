646fbf3c6972eba3a257f6ebd5ccc400d43fb627
Eduardo Feitosa <eduardofeitosa@Mac-mini-de-Eduardo.local>
2023-06-01 08:46:51 -0300

    configuração inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { catchError, of } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+
+export function initApp(http: HttpClient, translate: TranslateService) {
+  return () => new Promise<boolean>((resolve: (res: boolean) => void) => {
+    const defaultLocale = 'pt';
+    http.get('/assets/i18n/dev.json').pipe(
+      catchError(() => of(null))
+    ).subscribe((devKeys: any) => {
+      translate.setTranslation(defaultLocale, devKeys || {});
+      translate.setDefaultLang(defaultLocale);
+      translate.use(defaultLocale);
+      resolve(true);
+    });
+  });
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot()
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: initApp,
+    deps: [ HttpClient, TranslateService ],
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
